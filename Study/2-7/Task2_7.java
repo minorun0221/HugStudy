50266d4bff4fe1ae3b4b6a6b104f68713abc7482
minorun0221 <minorun0221@hotmail.co.jp>
2025-11-23 11:01:51 +0900

    2-7修正

--- Study/2-7/Task2_7.java
+++ Study/2-7/Task2_7.java
@@ -8,14 +8,14 @@ public class Task2_7 {
 
        
        // ② ①で作成した配列の要素数を出力して下さい。
-       System.out.println(+ country.length);
+       System.out.println(country.length);
 
         /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
         *   1番目（先頭）の要素に 「りんご」 を代入
         *   2番目の要素に 「もも」 を代入
         *   3番目の要素に 「ぶどう」 を代入
         */
-       String[] strArray = new String[4];
+       String[] strArray = new String[3];
        strArray[0] = "りんご";
        strArray[1] = "もも";
        strArray[2] = "ぶどう";
@@ -26,12 +26,12 @@ public class Task2_7 {
 
 
         /* ⑤下記の処理について、何をしているのかコメントを記入して下さい。
-        *  [intクラスの配列 intArray を定義]
+        *  [10, 20, 30, 40, 50 を要素の値（初期値）とする、要素数 5 のintArray という名前のint型配列を宣言。]
         */
         int[] intArray = { 10, 20, 30, 40, 50 };
 
         // ⑥下記の処理について、何をしているのかコメントを記入して下さい。
-        // [添え字（インデックス）が 0と4 の要素の合計値を表示]
+        // [添え字（インデックス）が 0の要素10 と4の要素50 の合計値を表示]
         System.out.println(intArray[0] + intArray[4]);
 
 
