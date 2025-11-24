bfb3cce4c917ebf352389e785ffbfdd2b6b619e8
minorun0221 <minorun0221@hotmail.co.jp>
2025-11-24 12:36:25 +0900

    2-9修正

--- Study/2-9/Task2_9.java
+++ Study/2-9/Task2_9.java
@@ -46,25 +46,25 @@ public class Task2_9 {
     // 定数を使用して「 ログイン成功です。 」と出力して下さい。
 
     if (name.equals("alice") && pass.equals("alice123")) {
-        System.out.println( "ログイン成功です。" );
+        System.out.println(CONST_MSG_SUCCESS);
 
     // ② 「USER_NAME」の値のみ等しい場合。
     // 定数を使用して「 パスワードに誤りがあります。 」 と出力して下さい。
 
     } else if (name.equals("alice")) {
-      System.out.println( "パスワードに誤りがあります。");
+      System.out.println(CONST_MSG_ERROR_PASS);
 
     // ③ 「USER_PASSWORD」の値のみ等しい場合。
     //定数を使用して「 名前に誤りがあります。 」と出力して下さい。
 
     } else if (pass.equals("alice123")) {
-      System.out.println( "名前に誤りがあります。");
+      System.out.println(CONST_MSG_ERROR_NAME);
 
     // ④ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。
     //定数を使用して「 入力情報に誤りがあります。 」と出力して下さい。
 
     } else {
-        System.out.println( "入力情報に誤りがあります。" );
+        System.out.println(CONST_MSG_ERROR_INPUT);
     
     }
 
